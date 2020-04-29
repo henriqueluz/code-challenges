f6406b45b79396582092e22edcd1e2588aef26a4
Henrique Luz <domingos_luz@epam.com>
2020-04-29 09:48:56 +0200

    Solving Tower of Hanoi problem

--- src/test/java/com/github/henriqueluz/challenges/recursion/hanoi/Hanoi.java
+++ src/test/java/com/github/henriqueluz/challenges/recursion/hanoi/Hanoi.java
@@ -3,7 +3,14 @@ package com.github.henriqueluz.challenges.recursion.hanoi;
 public class Hanoi {
 
     public static void move(Integer numberOfDiscs, String from, String intermediate, String to) {
-
+        String message = String.format("Moving disc #%s from [%s] to [%s]", numberOfDiscs, from, to);
+        if (numberOfDiscs.equals(1)) {
+            System.out.println(message);
+        } else {
+            move(numberOfDiscs - 1, from, to, intermediate);
+            System.out.println(message);
+            move(numberOfDiscs - 1, intermediate, from, to);
+        }
     }
 
 }
--- src/test/java/com/github/henriqueluz/challenges/recursion/hanoi/HanoiTest.java
+++ src/test/java/com/github/henriqueluz/challenges/recursion/hanoi/HanoiTest.java
@@ -6,6 +6,21 @@ public class HanoiTest {
 
     @Test
     public void moveOneDisc() {
+        Hanoi.move(1,  "A", "B","C");
+    }
 
+    @Test
+    public void moveTwoDiscs() {
+        Hanoi.move(2,  "A", "B","C");
+    }
+
+    @Test
+    public void moveThreeDiscs() {
+        Hanoi.move(3,  "A", "B","C");
+    }
+
+    @Test
+    public void moveTenDiscs() {
+        Hanoi.move(10,  "A", "B","C");
     }
 }
